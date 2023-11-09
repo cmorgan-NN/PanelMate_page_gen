71f616ae53b0b1adfffc71f87efe1c26ffa7ba29
cmorgan-NN <cmorgan@nationalnonwovens.com>
2023-11-09 12:00:46 -0500

    put in handling for font double high + 0/1 vis exp

--- Static_Graphic_Data_Text_Elements_Parser.py
+++ Static_Graphic_Data_Text_Elements_Parser.py
@@ -40,22 +40,29 @@ class Static_Graphic_Data_Text:
 
                 if (member == 'Visibility Expression' and 
                     visualOrder['Enable Cond. Visibility'] == 'No'):
-                    index_modifier =+ 1 
+                    index_modifier += 1 
                     parsed_element = { member : ''}
-
-                    #TODO: sometimes visibility expressions have more than just a reference in them,
-                    # so they need to be handled separately
-
+                elif (member == 'Visibility Expression' and
+                    #NOTE: sometimes visibility expressions have more than just a reference in them,
+                    # so they need to be run twice. This may need a better fix in the future
+                    #NOTE: copilot mostly wrote the 4 lines below and the Double High font code below that. Neat...
+                    # we only want to pass along the '] =?' part of the expression until render
+                    # where it is rendered out as logic and changed from 0's and 1's to 
+                    # True and False, respectively
+                      '] =' in line):
+                    next_parsed_element = Sub_Parsers.table_parser(member, index + 1, line)
+                    parsed_element = Sub_Parsers.table_parser(member, index, line)
+                    parsed_element = { member : parsed_element.get(member) + ' ' + next_parsed_element.get(member)}
+                    index_modifier -= 1 #to manipulate index for extra word
                 elif member != 'Text':    
                     parsed_element = Sub_Parsers.table_parser(member, index, line)
                     if member == 'Font' and parsed_element.get(member) == 'Double':
-                        #since parsing trips on the space, doube high font needs to be
-                        #handled separately
-                        parsed_element = { member : 'Double High'}
-                        index_modifier =+ 1
+                        #since parsing trips on the space, double high font needs to be handled separately
+                        next_parsed_element = Sub_Parsers.table_parser(member, index + 1, line)
+                        parsed_element = { member : parsed_element.get(member) + ' ' + next_parsed_element.get(member)}
+                        index_modifier -= 1 #to manipulate index for extra word
                 elif member == 'Text':
                     parsed_element = { member : line[current_line_pos - 1:].strip() }
-
                 visualOrder.update(parsed_element)
 
                 #Accumulate number of chars before the Text element
